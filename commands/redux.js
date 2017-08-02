546985e23a335a501b9cb821c91d8fe724d615ab
Justin Lane <justin@justinlane.org>
2017-08-03 09:26:15 +1200

    More Saga Tweaks

--- commands/redux.js
+++ commands/redux.js
@@ -2,8 +2,8 @@
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, ignite, strings, print } = context
-  const { isBlank, pascalCase } = strings
+  const { parameters, ignite, strings, print, filesystem } = context
+  const { isBlank, pascalCase, lowerCase } = strings
   const config = ignite.loadIgniteConfig()
 
   // validation
@@ -14,15 +14,49 @@ module.exports = async function (context) {
   }
 
   const name = pascalCase(parameters.first)
+  const lowerName = lowerCase(parameters.first)
   const props = { name }
 
   const jobs = [{ template: `redux.ejs`, target: `src/Redux/${name}Redux.js` }]
   if (config.tests) {
     jobs.push({
-      template: `redux-test-${config.tests}.ejs`,
+      template: `redux-test.ejs`,
       target: `src/__tests__/Redux/${name}ReduxTest.js`
     })
   }
 
   await ignite.copyBatch(context, jobs, props)
+
+  // Patch in redux's index.js
+  const reduxName = name
+  const indexFilePath = `${process.cwd()}/src/Redux/index.js`
+  const toAdd = `    ${lowerName}: require('./${name}Redux').reducer,`
+  const sagaFilePath = `${process.cwd()}/src/Sagas/index.js`
+  const importToAdd = `import { ${reduxName}Types } from '../Redux/${reduxName}Redux'`
+
+  if (!filesystem.exists(indexFilePath)) {
+    const msg = `No '${indexFilePath}' file found.  Can't add to index.js.`
+    print.error(msg)
+    process.exit(1)
+  }
+
+  // insert redux require
+  ignite.patchInFile(indexFilePath, {
+    after: "const rootReducer = combineReducers",
+    insert: toAdd
+  })
+
+
+  if (!filesystem.exists(sagaFilePath)) {
+    const msg = `No '${sagaFilePath}' file found.  Can't add to index.js.`
+    print.error(msg)
+    process.exit(1)
+  }
+
+  // insert saga import
+  ignite.patchInFile(sagaFilePath, {
+    after: "// Types",
+    insert: importToAdd
+  })
+
 }
--- commands/saga.js
+++ commands/saga.js
@@ -2,10 +2,9 @@
 
 module.exports = async function (context) {
   // grab some features
-  const { parameters, ignite, print, strings } = context
-  const { pascalCase, isBlank } = strings
+  const { parameters, ignite, print, strings, filesystem } = context
+  const { pascalCase, isBlank, upperCase, snakeCase } = strings
   const config = ignite.loadIgniteConfig()
-  const { tests } = config
 
   // validation
   if (isBlank(parameters.first)) {
@@ -15,16 +14,58 @@ module.exports = async function (context) {
   }
 
   const name = pascalCase(parameters.first)
-  const props = { name }
+  const upperName = upperCase(parameters.first)
+  const props = { name, upperName }
 
-  const jobs = [{ template: `saga.ejs`, target: `App/Sagas/${name}Sagas.js` }]
-  if (tests) {
+  const jobs = [{ template: `saga.ejs`, target: `src/Sagas/${name}Sagas.js` }]
+  if (config.tests) {
     jobs.push({
-      template: `saga-test-${tests}.ejs`,
+      template: `saga-test.ejs`,
       target: `src/__tests__/Saga/${name}SagaTest.js`
     })
   }
 
   // make the templates
   await ignite.copyBatch(context, jobs, props)
+
+  // Patch in redux's index.js
+  const sagaName = name
+  const indexFilePath = `${process.cwd()}/src/Sagas/index.js`
+  const fixtureFilePath = `${process.cwd()}/src/Services/FixtureApi.js`
+  const importToAdd = `import { post${sagaName}, get${sagaName}, update${sagaName}, delete${sagaName}, all${sagaName} } from './${sagaName}Sagas'`
+
+  if (!filesystem.exists(indexFilePath)) {
+    const msg = `No '${indexFilePath}' file found.  Can't add to index.js.`
+    print.error(msg)
+    process.exit(1)
+  }
+
+  // insert saga import
+  ignite.patchInFile(indexFilePath, {
+    after: "// Sagas",
+    insert: importToAdd
+  })
+
+  ignite.patchInFile(indexFilePath, {
+    after: "    // some sagas only receive an action",
+    insert: `    takeLatest(${sagaName}Types.${upperName}_REQUEST, get${sagaName}, api),
+    takeLatest(${sagaName}Types.${upperName}_ATTEMPT, post${sagaName}, api),
+    takeLatest(${sagaName}Types.${upperName}_UPDATE, update${sagaName}, api),
+    takeLatest(${sagaName}Types.${upperName}_DELETE, delete${sagaName}, api),`
+  })
+
+
+  if (!filesystem.exists(fixtureFilePath)) {
+    const msg = `No '${fixtureFilePath}' file found.  Can't add to index.js.`
+    print.error(msg)
+    process.exit(1)
+  }
+
+  ignite.patchInFile(fixtureFilePath, {
+    after: "// Functions return fixtures",
+    insert: `  post${sagaName}: () => { return { ok: true, data: "21" } },
+  get${sagaName}: () => { return { ok: true, data: "21" } },
+  patch${sagaName}: () => { return { ok: true, data: "21" } },
+  delete${sagaName}: () => { return { ok: true } },`
+  })
 }
