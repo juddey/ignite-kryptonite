3738d2dcbcc8c03a091011af9f2a4ba242831b45
Justin Lane <justin@justinlane.org>
2018-05-25 13:41:34 +1200

    Update CreateStore to Remove autorehydrate function

--- boilerplate/src/Redux/CreateStore.js
+++ boilerplate/src/Redux/CreateStore.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { autoRehydrate } from 'redux-persist'
 import Config from '../Config/DebugConfig'
 import createSagaMiddleware from 'redux-saga'
 import RehydrationServices from '../Services/RehydrationServices'
@@ -23,13 +22,6 @@ export default (rootReducer, rootSaga) => {
   /* ------------- Assemble Middleware ------------- */
 
   enhancers.push(applyMiddleware(...middleware))
-
-  /* ------------- AutoRehydrate Enhancer ------------- */
-
-  // add the autoRehydrate enhancer
-  if (ReduxPersist.active) {
-    enhancers.push(autoRehydrate())
-  }
 
   // if Reactotron is enabled (default for __DEV__), we'll create the store through Reactotron
   const createAppropriateStore = Config.useReactotron
